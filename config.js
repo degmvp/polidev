b7265f971e1fe74172e56bea5f6f06f02c0a7b0b
degrust <degrust@gmail.com>
2026-04-20 09:45:58 -0300

    corrigindo chave supabase

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://vdpucrilxzlnxxnaohdh.supabase.co"
-const SUPABASE_KEY = "sb_publishable_iCOZMw9xc0QZaGQWTHdxMQ_O33nfE42"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkcHVjcmlseHpsbnh4bmFvaGRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0OTc0NzIsImV4cCI6MjA4ODA3MzQ3Mn0.hdGtfK0eQf0-Iu_LqYqKKLvt8189euaGy-mmGMALJ_I"
 
 const { createClient } = supabase
 const db = createClient(SUPABASE_URL, SUPABASE_KEY)
