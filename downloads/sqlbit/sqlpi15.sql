03db0556c58286fa409e07d74dec5e9de47f6c03
degrust <degrust@gmail.com>
2026-06-11 20:04:02 -0300

    sqlpi15 11/06/2026

---
+++ downloads/sqlbit/sqlpi15.sql
@@ -0,0 +1,13 @@
+15 - Conclusão
+
+Entender:
+
+Páginas
+Registros
+Offsets
+Slot Array
+Row Overflow
+
+permite projetar bancos mais eficientes e identificar gargalos invisíveis para a maioria dos desenvolvedores.
+
+O verdadeiro desempenho começa na forma como os dados são armazenados.
