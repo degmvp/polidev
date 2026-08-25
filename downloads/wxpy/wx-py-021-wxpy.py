58368e8efda93632127f3b9359a5989c07a696bc
degrust <degrust@gmail.com>
2026-08-25 10:20:40 -0300

    wxpy   25/08/2026

---
+++ downloads/wxpy/wx-py-021-wxpy.py
@@ -0,0 +1,11 @@
+# ==========================================================
+# POLYDEV WX — ARQUIVO DE HOMOLOGAÇÃO
+# Programa: wx-py-021-wxpy.py
+# Bloco: WXPY02
+# ==========================================================
+
+print("POLYDEV WX - HOMOLOGACAO")
+print("Bloco: WXPY02")
+print("Programa: 021")
+print("Arquivo: wx-py-021-wxpy.py")
+print("Teste de paginacao logica OK")
---
+++ downloads/wxpy/wx-py-029-wxpy.py
@@ -0,0 +1,12 @@
+# ==========================================================
+# POLYDEV WX — ARQUIVO DE HOMOLOGAÇÃO
+# Programa: 
+
+# Bloco: WXPY02
+# ==========================================================
+
+print("POLYDEV WX - HOMOLOGACAO")
+print("Bloco: WXPY02")
+print("Programa: 021")
+print("Arquivo: wx-py-021-wxpy.py")
+print("Teste de paginacao logica OK")
